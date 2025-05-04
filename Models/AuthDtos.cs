c663bfcc5a2f7f6c36e04dfb1d43b7ee274ebbf9
abramepithey <abramepithey@gmail.com>
2025-05-04 15:14:04 -0600

    Add AuthDtos for user registration and login models

---
+++ Models/AuthDtos.cs
@@ -0,0 +1,13 @@
+namespace MinimalHabitsBlazor.Models;
+
+public class RegisterDto
+{
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
+
+public class LoginDto
+{
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+} 
