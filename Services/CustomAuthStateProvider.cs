63addb0dc3851e956a1b4f5e9cf42c7ca0080767
abramepithey <abramepithey@gmail.com>
2025-05-05 17:26:16 -0600

    Add CustomAuthStateProvider for managing user authentication state

---
+++ Services/CustomAuthStateProvider.cs
@@ -0,0 +1,57 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace MinimalHabitsBlazor.Services;
+
+public class CustomAuthStateProvider : AuthenticationStateProvider
+{
+    private readonly IJSRuntime _jsRuntime;
+    private readonly AuthService _authService;
+
+    public CustomAuthStateProvider(IJSRuntime jsRuntime, AuthService authService)
+    {
+        _jsRuntime = jsRuntime;
+        _authService = authService;
+    }
+
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        var token = await _authService.GetTokenAsync();
+        
+        if (string.IsNullOrEmpty(token))
+        {
+            return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+        }
+
+        var claims = ParseClaimsFromJwt(token);
+        var identity = new ClaimsIdentity(claims, "jwt");
+        var user = new ClaimsPrincipal(identity);
+        
+        return new AuthenticationState(user);
+    }
+
+    public void NotifyUserAuthentication(string token)
+    {
+        var claims = ParseClaimsFromJwt(token);
+        var identity = new ClaimsIdentity(claims, "jwt");
+        var user = new ClaimsPrincipal(identity);
+        
+        NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
+    }
+
+    public void NotifyUserLogout()
+    {
+        var identity = new ClaimsIdentity();
+        var user = new ClaimsPrincipal(identity);
+        
+        NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
+    }
+
+    private IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+    {
+        var handler = new JwtSecurityTokenHandler();
+        var token = handler.ReadJwtToken(jwt);
+        return token.Claims;
+    }
+} 
