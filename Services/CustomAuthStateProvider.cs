a1843d43d6168dc4be89b914b71ff5ec5ee3844b
abramepithey <abramepithey@gmail.com>
2025-05-07 10:40:49 -0600

    Add user model and integrate authentication packages for user management

--- Services/CustomAuthStateProvider.cs
+++ Services/CustomAuthStateProvider.cs
@@ -1,6 +1,7 @@
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.JSInterop;
 
 namespace MinimalHabitsBlazor.Services;
 
