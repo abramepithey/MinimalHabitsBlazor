e5aa45420e246ec95edbfe19595fdf4cb55d1d87
abramepithey <abramepithey@gmail.com>
2025-05-08 17:01:10 -0600

    Ensure ApiBaseUrl is configured in appsettings.json, throwing an exception if not set

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-var apiBaseUrl = builder.Configuration["ApiBaseUrl"];
+var apiBaseUrl = builder.Configuration["ApiBaseUrl"] ?? throw new InvalidOperationException("ApiBaseUrl is not configured in appsettings.json");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiBaseUrl) });
 builder.Services.AddScoped<AuthService>();
 builder.Services.AddScoped<HabitService>();
